f5cab05e4b14271b84eea0d3ed1b392af576e962
felipelinodev <felipelinodesigndoe@gmail.com>
2025-08-03 17:12:34 -0300

    adicionando-rotas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 //Componets
 import CriarCard from './components/CriarCard/CriarCard';
 import Card from './components/Card/Card';
 import CaixaDeCards from './components/CaixaDeCards/CaixaDeCards';
+import EditCard from './pages/EditCard/EditCard';
 
 function App() {
 
   const [cards, setCards] = useState([])
   const [dados, setTdados] = useState({})
-  
+
   const [cardDeletar, setCardDeletar] = useState()
 
   const [corDeCriacao, setCorDeCriacao] = useState("")
@@ -27,30 +30,35 @@ function App() {
 
   const removerTarefa = (id) => {
     setCards((prevState) => prevState.filter((card) => card.id !== id))
-  } 
+  }
 
   console.log(cardDeletar)
 
   return (
     <div className="App">
-      {/* <EntradaCampoCriar setValorInput={handleSetValor}/> */}
-      <CriarCard setTdados={setTdados} />
-      <CaixaDeCards>
+      <BrowserRouter>
 
-        {cards.map((tarefa) => (
-          <Card 
-            id={tarefa.id}
-            key={tarefa.id} 
-            removerTarefa={removerTarefa} 
-            titulo={tarefa.titulo} 
-            subtitulo={tarefa.subtitulo} 
-            corDeCriacao={corDeCriacao} />
-        ))}
-
-        
-
-      </CaixaDeCards>
-
+        <Routes>
+          <Route path='/' element={
+            <>
+              {/* <EntradaCampoCriar setValorInput={handleSetValor}/> */}
+              <CriarCard setTdados={setTdados} />
+              <CaixaDeCards>
+                {cards.map((tarefa) => (
+                  <Card
+                    id={tarefa.id}
+                    key={tarefa.id}
+                    removerTarefa={removerTarefa}
+                    titulo={tarefa.titulo}
+                    subtitulo={tarefa.subtitulo}
+                    corDeCriacao={corDeCriacao} />
+                ))}
+              </CaixaDeCards>
+            </>
+          } />
+          <Route path='/edit' element={<EditCard cards={cards} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/EditCard/EditCard.js
@@ -0,0 +1,12 @@
+
+
+const EditCard = () => {    
+
+  return (
+    <div>
+        <h1>Editar Card</h1>
+    </div>
+  )
+}
+
+export default EditCard
